413de56c81435778386330474d02684824ffa92a
vitoriomexas311 <mexas.v@northeastern.edu>
2025-07-04 11:21:48 -0400

    Update index.tsx

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -7,6 +7,10 @@ import Courses from "./Courses";
 export default function Kambaz() {
   return (
     <div id="wd-kambaz">
+      <div id="my-name-and-section">
+        <h1> Vitorio Mexas </h1>
+        <h2> CS 5610 Summer 2 | 7PM EST Tuesdays & Thursdays </h2>
+      </div>
       <table>
         <tr>
           <td valign="top">
