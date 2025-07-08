388e9502d93435ea36b21a01177efc498b80c430
vitoriomexas311 <mexas.v@northeastern.edu>
2025-07-08 10:13:28 -0400

    Lab2 stuff, now onto kambaz

---
+++ src/Labs/Lab2/Float.tsx
@@ -0,0 +1,81 @@
+export default function Float() {
+  return (
+    <div id="wd-float-divs">
+      <h2>Float</h2>
+      <div>
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+        <img
+          className="wd-float-left"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+        <img
+          className="wd-float-left"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+        <div className="wd-float-done"></div>
+      </div>
+      <div id="wd-float-divs">
+        <h2>Float</h2>
+        <div>
+          <div className="wd-float-left wd-dimension-portrait wd-bg-color-yellow">
+            Yellow{" "}
+          </div>
+          <div className="wd-float-left wd-dimension-portrait wd-bg-color-blue wd-fg-color-white">
+            Blue{" "}
+          </div>
+          <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
+            Red{" "}
+          </div>
+          <img
+            className="wd-float-right"
+            src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+          />
+          <div className="wd-float-done"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,7 +1,119 @@
+import { Container } from "react-bootstrap";
+import BackgroundColors from "./BackgroundColors";
+import Borders from "./Borders";
+import Corners from "./Corners";
+import Dimensions from "./Dimensions";
+import Flex from "./Flex";
+import Float from "./Float";
+import ForegroundColors from "./ForegroundColors";
+import GridLayout from "./GridLayout";
+import "./index.css";
+import Margins from "./Margins";
+import Padding from "./Padding";
+import Positions from "./Positions";
+import ReactIconsSampler from "./ReactIcons";
+import ZIndex from "./zIndex";
+import BootStrapGrids from "./BootStrapGrids";
+import ScreenSizeLabel from "./ScreenSizeLabel";
+import BootStrapTables from "./BootStrapTables";
+import BootStrapLists from "./BootStrapLists";
+import BootStrapForms from "./BootStrapForms";
+import BootStrapNavigation from "./BootStrapNavigation";
+
 export default function Lab2() {
   return (
-    <div>
-      <h2>Lab 2</h2>
-    </div>
+    <Container>
+      <div id="wd-lab2">
+        <h2>Lab 2 - Cascading Style Sheets</h2>
+        <h3>Styling with the STYLE attribute</h3>
+        {/* We can use the style attribute to style the element from index.css */}
+        <div id="wd-css-id-selectors">
+          <h3>ID selectors</h3>
+          <p id="wd-id-selector-1">
+            Instead of changing the look and feel of all the elements of the
+            same name, e.g., P, we can refer to a specific element by its ID
+          </p>
+          <p id="wd-id-selector-2">
+            Here's another paragraph using a different ID and a different look
+            and feel
+          </p>
+        </div>
+        {/* Class selectors  as opposed to id selectors (groupings)*/}
+        <div id="wd-css-class-selectors">
+          <h3>
+            Class selectors, groupings of elements as opposed to specific id's
+          </h3>
+
+          <p className="wd-class-selector">
+            Instead of using IDs to refer to elements, you can use an element's
+            CLASS attribute
+          </p>
+
+          <h4 className="wd-class-selector">
+            This heading has same style as paragraph above
+          </h4>
+        </div>
+        <div id="wd-css-document-structure">
+          <div className="wd-selector-1">
+            <h3>Document structure selectors</h3>
+            <div className="wd-selector-2">
+              Selectors can be combined to refer elements in particular places
+              in the document
+              <p className="wd-selector-3">
+                This paragraph's red background is referenced as
+                <br />
+                .selector-2 .selector3
+                <br />
+                meaning the descendant of some ancestor.
+                <br />
+                <span className="wd-selector-4">
+                  Whereas this span is a direct child of its parent
+                </span>
+                <br />
+                You can combine these relationships to create specific styles
+                depending on the document structure
+              </p>
+            </div>
+          </div>
+        </div>
+        <h1>Foreground Colors</h1>
+        <ForegroundColors />
+        <h1>Background Colors</h1>
+        <BackgroundColors />
+        <h1>Borders</h1>
+        <Borders />
+        <h1>Padding</h1>
+        <Padding />
+        <h1>Margins</h1>
+        <Margins />
+        <h1>Rounded Corners</h1>
+        <Corners />
+        <h1>Dimensions</h1>
+        <Dimensions />
+        <h1>Positions</h1>
+        <Positions />
+        <h1>Z Index</h1>
+        <ZIndex />
+        <h1>Float</h1>
+        <Float />
+        <h1>Grid Layout</h1>
+        <GridLayout />
+        <h1>Flex</h1>
+        <Flex />
+        <h1>React Icons</h1>
+        <ReactIconsSampler />
+        <h1>BootStrap Grids</h1>
+        <BootStrapGrids />
+        <h1>Screen Size Label</h1>
+        <ScreenSizeLabel />
+        <h1>BootStrap Tables</h1>
+        <BootStrapTables />
+        <h1>BootStrap Lists</h1>
+        <BootStrapLists />
+        <h1>BootStrap Forms</h1>
+        <BootStrapForms />
+      </div>
+      <BootStrapNavigation />
+    </Container>
   );
 }
